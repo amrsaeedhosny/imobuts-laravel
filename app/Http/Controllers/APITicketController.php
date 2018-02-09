5a95cadbb321cf02d9f8b077fdb7ddd4fb9da3e5
ashrakat <ashrakat.mokhtar@yahoo.com>
2018-02-10 00:05:51 +0200

    Delete old

---
+++ app/Http/Controllers/APITicketController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class APITicketController extends Controller
+{
+	public function getTickets(Request $request){
+
+	}
+
+	public function getTicketDetails(Request $request){
+
+	}
+}
