6fa0d602d347dd05b7a8078b1fdb5544b699898b
ashrakat <ashrakat.mokhtar@yahoo.com>
2018-03-03 17:53:41 +0200

    view Tickets and viewOneTicketDetails

--- app/Http/Controllers/APITicketController.php
+++ app/Http/Controllers/APITicketController.php
@@ -1,3 +1,4 @@
+
 <?php
 
 namespace App\Http\Controllers;
@@ -7,10 +8,31 @@ use Illuminate\Http\Request;
 class APITicketController extends Controller
 {
 	public function getTickets(Request $request){
-
+     $tickets = Tickets::where('PassengerID' ,'=', $request->id)->get();   // id here means user id  
+     $response = array('response' => [], 'success'=>true);    
+     if ($tickets)
+      {
+       $response['response'] = $tickets;
+      }
+     else
+      {
+       $response['success'] = false;
+      }            
+     return response()->json($response);    
+         
 	}
 
 	public function getTicketDetails(Request $request){
-
+      $ticket = Ticket::find($request->id)                                // id here means ticket id
+      $response = array('response' => [], 'success'=>true);    
+     if ($ticket)
+      {
+       $response['response'] = $ticket;
+      }
+     else
+      {
+       $response['success'] = false;
+      }            
+     return response()->json($response);
 	}
 }
