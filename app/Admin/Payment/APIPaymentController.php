099bca6ac8b076b2370a07c01177105558bdb570
ashrakat <ashrakat.mokhtar@yahoo.com>
2018-07-14 15:02:26 +0200

    .

--- app/Admin/Payment/APIPaymentController.php
+++ app/Admin/Payment/APIPaymentController.php
@@ -1,30 +1,23 @@
 <?php
-
 namespace App\Admin\Payment;
-
 use App\Admin\Passengers\Passenger;
 use App\Admin\Tickets\Ticket;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
-
-
 class APIPaymentController extends Controller
 {
-	/**
-	 * @param  float $value the ticket cost
-	 * @param  float $balance the user current balance
-	 *
-	 * @return array
-	 */
+        /**
+        * @param  float $price the ticket cost
+        * @param  string $token The unique token of the user
+        * @return array
+        */
 	public function buyTicket( Request $request ) {
 		$validator = Validator::make( $request->toArray(), [
 			'token' => 'required|exists:passengers',
 			'price' => 'required',
 		] );
-
-
 		$response = array( 'response' => new \stdClass(), 'success' => true );
 		if ( $validator->fails() ) {
 			$errors = $validator->errors();
@@ -35,22 +28,19 @@ class APIPaymentController extends Controller
 				$response['response']->price = $errors->first( 'price' );
 			}
 			$response['success'] = false;
-
 			return response()->json( $response );
 		}
-
 		$price     = $request->input( 'price' );
 		$passenger = Passenger::where( 'token', $request->input( 'token' ) )->first();
-
-
 		if ( $passenger && ( $passenger->balance - $price >= 0 ) ) {      // in IoT machine checks if price is less than balance as well
 			/*  generate ticket for that user  */
 			$ticket              = new Ticket();
 			$ticket->price       = $price;
-			$ticket->date        = Carbon::now();
-			$date                = Carbon::now();
+			$ticket->date        = Carbon::now()->toDateTimeString();
+			$date                = Carbon::now()->toDateTimeString();
 			$ticket->passengerID = $passenger->id;
 			//$ticket->code = bcrypt(($passenger->token).($ticket->date));
+			$j = 0;
 			for ( $i = 0; $i < 19; $i ++ ) {
 				if ( $date[ $i ] == ' ' || $date[ $i ] == '-' || $date[ $i ] == ':' ) {
 					$date[ $i ] = $passenger->token[ $j ];
@@ -59,17 +49,14 @@ class APIPaymentController extends Controller
 			}
 			$ticket->code = bcrypt( ( $date ) . ( $passenger->id ) );
 			$ticket->save();
-
 			/*  update passenger balance */
 			$passenger->balance = $passenger->balance - $price;
 			$passenger->update();
 			$response['response']->newTicket = "new ticket has been issued";
 		} else {
-			$response['success'] = false;
+			$response['response']->newTicket = "not enough balance";
+			$response['success']             = false;
 		}
-
 		return response()->json( $response );
 	}
-
-
 }
--- routes/web.php
+++ routes/web.php
@@ -10,29 +10,11 @@
 |
 */
 use Illuminate\Support\Facades\Route;
-<<<<<<< Updated upstream
 
 Route::get( '/', function () {
 	return redirect( '/admin' );
 } );
 
-=======
-Route::get( '/', function () {
-	return redirect( '/admin' );
-} );
->>>>>>> Stashed changes
 Route::get('docs', function(){
 	return View::make('docs.api.index');
 });
-<<<<<<< HEAD
-Route::group(['middleware'=>\App\Http\Middleware\PaymentMiddleware::class],function (){
-	Route::get( 'stripe', 'StripeController@getView' );
-	Route::post( 'stripe', 'StripeController@charge' )->name( 'charge' );
-<<<<<<< Updated upstream
-});
-=======
->>>>>>> 6ad19c51995a0da56ab1a4e2a7c7c5e6a1859857
-
-=======
-});
->>>>>>> Stashed changes
